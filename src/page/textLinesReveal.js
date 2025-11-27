4233de8c123977bfd1210cc9dd5132d7edcb99c0
Shidqi <shidqi@Shidqis-MacBook-Pro.local>
2025-11-27 11:51:04 +0700

    fix(text-reveal): add text reveal to only run on desktop

--- src/page/textLinesReveal.js
+++ src/page/textLinesReveal.js
@@ -12,59 +12,70 @@ function setupTextLinesReveal() {
 
   const mm = gsap.matchMedia();
 
-  textEls.forEach((text) => {
-    SplitText.create(text.childNodes, {
-      type: "words, chars",
-      wordsClass: "word",
-      charsClass: "char",
-      onSplit(self) {
-        return mm.add("min-width: 992px", () => {
-          gsap
-            .timeline({
-              scrollTrigger: {
-                trigger: text,
-                start: "top bottom",
-                end: "top 80%",
-                toggleActions: "play none none none",
-              },
-              defaults: {
-                ease: "power3",
-              },
-            })
-            .set(self.words, {
-              willChange: "transform, opacity, filter",
-            })
-            .set(self.chars, {
-              willChange: "color",
-            })
-            .from(self.words, {
-              yPercent: 110,
-              filter: "blur(20px)",
-              autoAlpha: 0,
-              delay: 0.2,
-              duration: 0.8,
-              stagger: { each: 0.1 },
-            })
-            .to(
-              self.chars,
-              {
-                keyframes: [
-                  { color: BLUE_500, duration: 0.2 },
-                  { color: BRAND_COLOR, duration: 0.2 },
-                  { color: "inherit", duration: 0.4 },
-                ],
-                duration: 0.8,
-                stagger: { each: 0.01 },
-              },
-              "<",
-            );
-        });
-      },
-    });
-  });
+  mm.add("(min-width: 992px)", () => {
+    textEls.forEach((text) => {
+      let split = new SplitText(text, {
+        type: "words, chars",
+        wordsClass: "word",
+        charsClass: "char",
+      });
 
-  gsap.set("[data-prevent-flicker='true']", {
-    visibility: "visible",
+      // Create animation
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: text,
+            start: "top bottom",
+            end: "top 80%",
+            toggleActions: "play none none none",
+          },
+          defaults: {
+            ease: "power3",
+          },
+        })
+        .set(split.words, {
+          willChange: "transform, opacity, filter",
+        })
+        .set(split.chars, {
+          willChange: "color",
+        })
+        .from(split.words, {
+          yPercent: 110,
+          filter: "blur(20px)",
+          autoAlpha: 0,
+          delay: 0.2,
+          duration: 0.8,
+          stagger: { each: 0.1 },
+        })
+        .to(
+          split.chars,
+          {
+            keyframes: [
+              {
+                color: typeof BLUE_500 !== "undefined" ? BLUE_500 : "blue",
+                duration: 0.2,
+              }, // Fallback if var undefined
+              {
+                color:
+                  typeof BRAND_COLOR !== "undefined" ? BRAND_COLOR : "black",
+                duration: 0.2,
+              },
+              { color: "inherit", duration: 0.4 },
+            ],
+            duration: 0.8,
+            stagger: { each: 0.01 },
+          },
+          "<",
+        );
+    });
+
+    gsap.set("[data-prevent-flicker='true']", {
+      visibility: "visible",
+    });
+
+    return () => {
+      gsap.set("[data-prevent-flicker='true']", { clearProps: "visibility" });
+    };
   });
 }
 
